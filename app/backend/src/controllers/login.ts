44c6bbe7c58c81a36a10394dc15ec89b886ba672
Douglas <douglasfmr1@gmail.com>
2022-06-16 17:51:29 -0300

    feat(login e middlewares): login e middlewares funcionando localmente

--- app/backend/src/controllers/login.ts
+++ app/backend/src/controllers/login.ts
@@ -5,7 +5,7 @@ const loginController: RequestHandler = async (req, res) => {
   const { email, password } = req.body;
   try {
     const result = await login(email, password);
-    res.status(200).json( result );
+    res.status(200).json(result);
   } catch (err) {
     res.status(401).json({ message: (err as Error).message });
   }
--- app/backend/src/routes/routes.ts
+++ app/backend/src/routes/routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import loginController from '../controllers/login';
+import middleLogin from '../middlewares/login';
 
 const router = Router();
 
-router.post('/login', loginController);
+router.post('/login', middleLogin, loginController);
 
 export default router;
